e6837ed7fb4b6409b55bbba29e7b513fe92097b1
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2018-10-29 20:31:15 +0000

    DUTweaks: Allow to doubletap or longpress power to toggle torch [3/3]
    Change-Id: Ifc6942f088d8697c9014f55e2cbf78f0542f4b3b
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- src/com/dirtyunicorns/tweaks/fragments/Buttons.java
+++ src/com/dirtyunicorns/tweaks/fragments/Buttons.java
@@ -42,7 +42,7 @@ import com.android.internal.logging.nano.MetricsProto;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
+import com.android.internal.util.du.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -55,7 +55,10 @@ public class Buttons extends SettingsPreferenceFragment
 
     private static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
+    private static final String DOUBLE_TAP_POWER_FLASHLIGHT = "double_tap_power_flashlight";
+
     private ListPreference mVolumeKeyCursorControl;
+    private ListPreference mDoubleTapPowerFlashlight;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -73,6 +76,17 @@ public class Buttons extends SettingsPreferenceFragment
            mVolumeKeyCursorControl.setValue(Integer.toString(volumeRockerCursorControl));
            mVolumeKeyCursorControl.setSummary(mVolumeKeyCursorControl.getEntry());
 	}
+
+        mDoubleTapPowerFlashlight =
+                (ListPreference) prefScreen.findPreference(DOUBLE_TAP_POWER_FLASHLIGHT);
+        if (deviceHasFlashlight()) {
+            mDoubleTapPowerFlashlight.setOnPreferenceChangeListener(this);
+            mDoubleTapPowerFlashlight.setValue(Integer.toString(Settings.Secure.getInt(getContext()
+                    .getContentResolver(), Settings.Secure.TORCH_POWER_BUTTON_GESTURE, 0)));
+            mDoubleTapPowerFlashlight.setSummary(mDoubleTapPowerFlashlight.getEntry());
+        } else {
+            prefScreen.removePreference(mDoubleTapPowerFlashlight);
+        }
     }
 
     @Override
@@ -88,8 +102,26 @@ public class Buttons extends SettingsPreferenceFragment
             mVolumeKeyCursorControl
                     .setSummary(mVolumeKeyCursorControl.getEntries()[volumeKeyCursorControlIndex]);
             return true;
+        } else if (preference == mDoubleTapPowerFlashlight) {
+            int torchPowerButtonValue = Integer.parseInt((String) newValue);
+            Settings.Secure.putInt(getContext().getContentResolver(),
+                    Settings.Secure.TORCH_POWER_BUTTON_GESTURE, torchPowerButtonValue);
+            int index = mDoubleTapPowerFlashlight.findIndexOfValue((String) newValue);
+            mDoubleTapPowerFlashlight.setSummary(
+                    mDoubleTapPowerFlashlight.getEntries()[index]);
+            if (torchPowerButtonValue == 1) {
+                // if doubletap for torch is enabled, switch off double tap for camera
+                Settings.Secure.putInt(getContext().getContentResolver(),
+                        Settings.Secure.CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED,
+                        1/*camera gesture is disabled when 1*/);
+            }
+            return true;
         }
         return false;
+    }
+
+    private boolean deviceHasFlashlight() {
+        return Utils.deviceHasFlashlight(getContext());
     }
 
     @Override
