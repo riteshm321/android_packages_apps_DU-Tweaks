0b9818021269e823a684c63d530467e597a73831
PMS22 <prathams99@rediff.com>
2019-12-16 00:29:44 -0700

    DUTweaks: Add Flashlight blink on incoming calls [2/3]
    - Thanks to shripal17 for suggesting list preference
      instead of a toggle
    - Add a device specific flashlight check
    
    Change-Id: I76e2eca2c66f0b5ac53a546fc35157d222dbbfd0
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- src/com/dirtyunicorns/tweaks/fragments/Notifications.java
+++ src/com/dirtyunicorns/tweaks/fragments/Notifications.java
@@ -37,6 +37,7 @@ import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.dirtyunicorns.tweaks.preferences.TelephonyUtils;
+import com.android.internal.util.du.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -51,16 +52,19 @@ public class Notifications extends SettingsPreferenceFragment
     private static final String LIGHTS_CATEGORY = "notification_lights";
     private static final String HEADS_UP_NOTIFICATIONS_ENABLED = "heads_up_notifications_enabled";
     private static final String PULSE_AMBIENT_LIGHT_COLOR = "pulse_ambient_light_color";
+    private static final String FLASHLIGHT_ON_CALL = "flashlight_on_call";
 
     private GlobalSettingMasterSwitchPreference mHeadsUpEnabled;
     private PreferenceCategory mLightsCategory;
     private ColorPickerPreference mEdgeLightColorPreference;
+    private ListPreference mFlashlightOnCall;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.notifications);
         PreferenceScreen prefScreen = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
 
         PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
         if (!TelephonyUtils.isVoiceCapable(getActivity())) {
@@ -90,6 +94,17 @@ public class Notifications extends SettingsPreferenceFragment
             mEdgeLightColorPreference.setSummary(edgeLightColorHex);
         }
         mEdgeLightColorPreference.setOnPreferenceChangeListener(this);
+
+        mFlashlightOnCall = (ListPreference) findPreference(FLASHLIGHT_ON_CALL);
+        Preference FlashOnCall = findPreference("flashlight_on_call");
+        int flashlightValue = Settings.System.getInt(getContentResolver(),
+                Settings.System.FLASHLIGHT_ON_CALL, 0);
+        mFlashlightOnCall.setValue(String.valueOf(flashlightValue));
+        mFlashlightOnCall.setSummary(mFlashlightOnCall.getEntry());
+        mFlashlightOnCall.setOnPreferenceChangeListener(this);
+        if (!Utils.deviceHasFlashlight(getActivity())) {
+            prefScreen.removePreference(FlashOnCall);
+        }
     }
 
     @Override
@@ -115,6 +130,13 @@ public class Notifications extends SettingsPreferenceFragment
             int intHex = ColorPickerPreference.convertToColorInt(hex);
             Settings.System.putInt(getContentResolver(),
                     Settings.System.PULSE_AMBIENT_LIGHT_COLOR, intHex);
+            return true;
+        } else if (preference == mFlashlightOnCall) {
+            int flashlightValue = Integer.parseInt(((String) newValue).toString());
+            Settings.System.putInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL, flashlightValue);
+            mFlashlightOnCall.setValue(String.valueOf(flashlightValue));
+            mFlashlightOnCall.setSummary(mFlashlightOnCall.getEntry());
             return true;
         }
         return false;
